f2104312eb95ba1af22d36017171c28bce21145f
xq890 <xq890@THINKPAD-X260>
2017-04-22 07:35:04 +0800

    na

--- DevCloud/webpack.config.ts
+++ DevCloud/webpack.config.ts
@@ -1,1 +1,13 @@
 ﻿
+import * as webpack from 'webpack'
+export default {
+    entry: ["App/main.ts"],
+    output: {
+        path:"wwwroot"
+    },
+    devtool: "#@cheap-source-map",
+    module: {
+        
+    }
+
+} as webpack.Configuration
